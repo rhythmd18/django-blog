55c5e13971129d33642246639d829cacd1a0efa9
Rhythm Dutta <duttarhythm18@gmail.com>
2026-02-09 07:16:13 +0530

    Added new views

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,23 @@
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, edit
 
 from .models import Post
 
-# Create your views here.
-class PostList(TemplateView):
+class BlogListView(ListView):
+    model = Post
     template_name = 'home.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        posts = Post.objects.all()
-        context['posts'] = posts
-        return context
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
 
+class BlogCreateView(edit.CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
 
-def post_detail(request: HttpRequest, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post})
+class BlogUpdateView(edit.UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
